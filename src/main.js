ba04091560a809fc6f22e9a9e80312d7f5518e5c
jmanu4245 <juanmanuelfloresdelacruz@gmail.com>
2024-10-05 15:32:28 +0200

    Click manager

--- src/main.js
+++ src/main.js
@@ -45,23 +45,7 @@ const scene = new THREE.Scene();
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-window.addEventListener('click', (event) => {
-    // Calcular las coordenadas del mouse en espacio NDC (-1 a 1)
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    // Proyectar un rayo desde la cámara hacia la escena, basado en la posición del mouse
-    raycaster.setFromCamera(mouse, camera);
-
-    // Calcular los objetos intersectados por el rayo
-    const intersects = raycaster.intersectObjects(scene.children);
-
-    // Si hay intersecciones
-    if (intersects.length > 0) {
-        // Cambiar el color del primer objeto intersectado (en este caso, el planeta)
-        intersects[0].object.material.color.set(Math.random() * 0xffffff);
-    }
-});
 
     // Animation loop
     function animate() {
