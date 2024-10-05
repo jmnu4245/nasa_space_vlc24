f9dd3b0c609ddf1f56dfa1eec22d6500705f9110
Torrente <torrentep18@gmail.com>
2024-10-05 12:11:51 +0200

    objeto_planeta

---
+++ Planeta.js
@@ -0,0 +1,20 @@
+const SCALE = 62;
+
+class Planeta {
+    constructor(tamaño, posicion, textura) {
+    this.tamaño = tamaño;
+    this.posicion = posicion;
+    this.textura = textura;
+    //Donde tamaño es un float, posición es un vector de tres componentes y textura es un string.
+    }
+    cambiarPosicion= function(nuevaPosicion)
+    {
+        if (nuevaPosicion.length == 3){
+            this.posicion = nuevaPosicion/SCALE
+            ;
+        }else{
+            console.log("Error: la nueva posición no tiene tres componentes");
+        }
+    }
+        
+}
