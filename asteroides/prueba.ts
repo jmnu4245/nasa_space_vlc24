5ad1ce646f8fa6674cf8a9debae217af6d84a79e
T1N8T <168031394+T1N8T@users.noreply.github.com>
2024-10-06 10:29:26 +0200

    Update prueba.ts
    he cambiado el codigo que no funcionaba

--- asteroides/prueba.ts
+++ asteroides/prueba.ts
@@ -1,7 +1,7 @@
 import { CelestialBody } from './operations';
 const fs = require('fs');
 
-function read_celestial_bodies(url:string , n:number){
+function read_celestial_bodies(url: string, n: number): CelestialBody[] {
     let celestialBodies: CelestialBody[] = [];
     // Leer el archivo JSON
     const data = fs.readFileSync(url, 'utf8');
@@ -9,14 +9,13 @@ function read_celestial_bodies(url:string , n:number){
     const bodies = JSON.parse(data);
 
     // Recorrer cada objeto del array
-    bodies.slice(0,n).forEach((body: { spkid:string; full_name:string; e:number; a:number; q:number; i:number; om:number; w:number; ma:number; tp:number; n:number}) => {
-        const astro:CelestialBody = new CelestialBody (body.spkid, body.full_name, body.e, body.a, body.q, body.i, body.om, body.w, body.ma, body.tp, body.n);
-        //console.log (astro);
+    bodies.slice(0, n).forEach((body: { spkid: string; full_name: string; e: number; a: number; q: number; i: number; om: number; w: number; ma: number; tp: number; n: number }) => {
+        const astro: CelestialBody = new CelestialBody(body.spkid, body.full_name, body.e, body.a, body.q, body.i, body.om, body.w, body.ma, body.tp, body.n);
         celestialBodies.push(astro);
-        console.log(celestialBodies)
-        return celestialBodies;
-    });  
+    });
+
+    return celestialBodies;
 }
 
-const datos:CelestialBody[]|void = read_celestial_bodies('comets2_ordenado.json',2) ;
+const datos: CelestialBody[] = read_celestial_bodies('comets2_ordenado.json', 2);
 console.log(datos);
