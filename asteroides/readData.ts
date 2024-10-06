78289a214131c466500b266051ba0ee0d8bd3c65
danikiwii <danielgg04@gmail.com>
2024-10-06 11:25:54 +0200

    codigo lectura de datos correcto

--- asteroides/readData.ts
+++ asteroides/readData.ts
@@ -1,7 +1,7 @@
-import { CelestialBody } from './operations';
+import { CelestialBody } from './CelestialBody';
 const fs = require('fs');
 
-function read_celestial_bodies(url:string , n:number){
+export function read_celestial_bodies(url:string , n:number){
     let celestialBodies: CelestialBody[] = [];
     // Leer el archivo JSON
     const data = fs.readFileSync(url, 'utf8');
@@ -13,10 +13,9 @@ function read_celestial_bodies(url:string , n:number){
         const astro:CelestialBody = new CelestialBody (body.spkid, body.full_name, body.e, body.a, body.q, body.i, body.om, body.w, body.ma, body.tp, body.n);
         //console.log (astro);
         celestialBodies.push(astro);
-        console.log(celestialBodies)
-        return celestialBodies;
+        //console.log(celestialBodies)
     });  
+    return celestialBodies;
 }
 
-const datos:CelestialBody[]|void = read_celestial_bodies('comets2_ordenado.json',2) ;
-console.log(datos);
+
